f05d7c9269a1041fdaa69bba2b774eba2ff97147
DizzyThermal <DizzyThermal@gmail.com>
2013-03-04 14:49:57 -0500

    JScrollPane is now populated with Labels and TextFields.. Rough implementation; will work more on it later

--- Michelizer/src/GUI.java
+++ Michelizer/src/GUI.java
@@ -12,20 +12,35 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSpinner;
-import javax.swing.SpinnerModel;
+import javax.swing.JTextField;
 import javax.swing.SpinnerNumberModel;
 
 public class GUI extends JFrame implements ActionListener
 {
-	GridLayout gl = new GridLayout(4,1);
-	JPanel p = new JPanel();
-	JPanel spPanel;
-	JButton calculate = new JButton("Calculate");
-	JScrollPane sp = new JScrollPane();
-	ArrayList<JLabel> labels;
-	ArrayList<JComboBox> comboBoxes;
-	ArrayList<JSpinner> spinners;
+	// Initial Settings
+	private static int dimensionCount = 3;
+	private static int clusterCount = 4;
 
+	// Parameter GridLayout and Panel
+	GridLayout gl = new GridLayout(4,1);
+	JPanel p = new JPanel(gl);
+	
+	// Point Field GridLayout, Panel, and ScrollPane
+	GridLayout spGridLayout = new GridLayout(5, 3);
+	JPanel spPanel = new JPanel(spGridLayout);
+	JScrollPane sp = new JScrollPane(spPanel);
+
+	// Calculate Button
+	JButton calculateButton = new JButton("Calculate");
+	JButton addButton = new JButton("+");
+	JButton removeButton = new JButton("-");
+
+	// Labels and Fields
+	ArrayList<JLabel> labels = new ArrayList<JLabel>();
+	ArrayList<JComboBox> comboBoxes = new ArrayList<JComboBox>();
+	ArrayList<JSpinner> spinners = new ArrayList<JSpinner>();
+
+	// Label Strings
 	String[] labelStrings 		= {"Algorithm", "Distance Type", "Dimensions", "Clusters"};
 	String[] algorithmStrings	= {"MST", "K-Means", "Z-Score"};
 	String[] distanceStrings	= {"Manhatten", "Euclidian"};
@@ -37,12 +52,6 @@ public class GUI extends JFrame implements ActionListener
 		fl.setAlignment(FlowLayout.LEFT);
 		setLayout(fl);
 
-		labels = new ArrayList<JLabel>();
-		comboBoxes = new ArrayList<JComboBox>();
-		spinners = new ArrayList<JSpinner>();
-		p = new JPanel();
-		p.setLayout(gl);
-
 		for(int i = 0; i < labelStrings.length; i++)
 			labels.add(new JLabel(labelStrings[i] + ":"));
 		
@@ -52,8 +61,8 @@ public class GUI extends JFrame implements ActionListener
 		spinners.add(new JSpinner(new SpinnerNumberModel(3, 1, 10, 1)));
 		spinners.add(new JSpinner(new SpinnerNumberModel(4, 1, 100, 1)));
 		
-		spinners.get(0).setValue(3);
-		spinners.get(1).setValue(4);
+		spinners.get(0).setValue(dimensionCount);
+		spinners.get(1).setValue(clusterCount);
 		
 		for(int i = 0; i < labels.size(); i++)
 		{
@@ -62,16 +71,30 @@ public class GUI extends JFrame implements ActionListener
 		}
 
 		sp.setPreferredSize(new Dimension(285, 200));
-		for(int i = 0; i < 4; i++)
-			sp.add(new JLabel("Hi: " + i));
-
 		add(p);
 		add(sp);
 		
 		JPanel pCalculate = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-		pCalculate.setPreferredSize(new Dimension(289, 50));
-		pCalculate.add(calculate);
+		
+		pCalculate.add(addButton);
+		pCalculate.add(removeButton);
+		pCalculate.add(calculateButton);
 		add(pCalculate);
+		
+		updateScrollPanelDimensions(10, dimensionCount);
+	}
+	
+	public void updateScrollPanelDimensions(int points, int dimensions)
+	{
+		spPanel.removeAll();
+		spGridLayout.setRows(points+1); // Plus Labels
+		spGridLayout.setColumns(dimensions);
+		for(int i = 0; i < dimensions; i++)
+			spPanel.add(new JLabel("D" + (i+1)), JLabel.CENTER);
+			
+		for(int i = 0; i < points; i++)
+			for(int j = 0; j < dimensions; j++)
+				spPanel.add(new JTextField());
 	}
 	
 	@Override
